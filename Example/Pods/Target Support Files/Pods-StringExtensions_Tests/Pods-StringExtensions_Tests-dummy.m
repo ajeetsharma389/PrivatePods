677e7d7c1387718040d8d9cbefbfb8846f909134
Ajeet <ajeetsharma389@gmail.com>
2020-04-13 16:58:23 +0530

    some files removed along with POD folder

--- Example/Pods/Target Support Files/Pods-StringExtensions_Tests/Pods-StringExtensions_Tests-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_StringExtensions_Tests : NSObject
-@end
-@implementation PodsDummy_Pods_StringExtensions_Tests
-@end
--- Example/Pods/Target Support Files/StringExtensions/StringExtensions-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_StringExtensions : NSObject
-@end
-@implementation PodsDummy_StringExtensions
-@end
